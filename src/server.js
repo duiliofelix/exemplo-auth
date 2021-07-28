03f7cd1379c7024f2fb628a2aaac6c1a2fbbf637
Duilio Felix <duiliofelix@gmail.com>
2021-07-28 20:42:34 -0300

    default port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 
 const server = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.use('/', routes);
 
