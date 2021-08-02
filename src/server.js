0dad5260dd4984859ed740eacee62f82cd0096ef
Duilio Felix <duiliofelix@gmail.com>
2021-08-02 19:33:12 -0300

    feat: adding winston

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const { PORT } = require('./config');
+const logger = require('./services/logger');
 
 const server = express();
 
@@ -8,5 +9,5 @@ server.use(express.json());
 server.use('/', routes);
 
 server.listen(PORT, () => {
-  console.log(`Listening at http://localhost:${PORT}`);
+  logger.info(`Listening at http://localhost:${PORT}`);
 });
